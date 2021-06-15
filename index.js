4b932c9921879c46ee1f8c92856efba633c58b9f
WojciechLisCode <wlis58372+code@gmail.com>
2021-06-15 09:40:11 +0200

    basic routes added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const candlesRouter = require("./routers/candles");
+const usersRouter = require("./routers/users");
+
 const PORT = 4000;
 
 const app = express();
@@ -7,6 +10,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("test reposne");
 });
+
+app.use("/candles", candlesRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
