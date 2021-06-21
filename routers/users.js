42f8bb293e85c8d828f27c96b1620839db123e1c
WojciechLisCode <wlis58372+code@gmail.com>
2021-06-21 13:03:11 +0200

    login and logout features added

--- routers/users.js
+++ routers/users.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
 
 const bodyParser = require("body-parser");
 
 const jsonParser = bodyParser.json();
+
+const { toJWT } = require("../auth/jwt");
 
 const User = require("../models").user;
 const Candle = require("../models").candle;
@@ -40,6 +44,29 @@ router.get("/:id", async (req, res) => {
   });
 
   res.status(200).send({ message: "ok", userDetails });
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(400)
+      .send({ message: "Please provide both email and password" });
+  }
+
+  const user = await User.findOne({ where: { email } });
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return res.status(400).send({
+      message: "User with that email not found or password incorrect",
+    });
+  }
+
+  delete user.dataValues["password"]; // don't send back the password hash
+
+  const token = toJWT({ userId: user.id });
+  return res.status(200).send({ token, ...user.dataValues });
 });
 
 module.exports = router;
